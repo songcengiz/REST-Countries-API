83a6597d0bf94e3fb16297ef8571e4b415f9254a
unknown <song.gul94@gmail.com>
2024-01-12 14:49:37 +0300

    search and filter added in the new folder.

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,11 @@
 import Countries from '../features/countries/Countries';
-import FilterCountries from '../features/countries/FilterCountries';
-import SearchCountries from '../features/countries/SearchCountries';
+import FilterCountries from '../features/newOverview/FilterCountries';
+import SearchCountries from '../features/newOverview/SearchCountries';
 
 function Home({ darkMode }) {
   return (
     <div>
-      <div className="m-auto md:my-14 px-9 lg:px-0">
+      <div className="m-auto px-9 md:my-14 lg:px-0">
         <div className="grid flex-1 grid-rows-2  justify-between gap-11 lg:flex">
           <SearchCountries darkMode={darkMode} />
           <FilterCountries darkMode={darkMode} />
