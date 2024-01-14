b014250b89e46f7c5451383079e3ec15ec8dc3fc
unknown <song.gul94@gmail.com>
2024-01-14 12:03:55 +0300

    changed index and vite.config.js files

--- vite.config.js
+++ vite.config.js
@@ -14,12 +14,5 @@ export default defineConfig({
         },
       },
     }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      },
-    },
-  },
+  ]
 });
